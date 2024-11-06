0ea85ef5150e0c26a105ebb3f0a6a841584ca376
Beatriz Pereira <beatriz.pereira@grupoassec.local>
2024-11-06 14:50:09 +0000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  base: '/portfolio/',
+
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name]-[hash].mjs',
+        chunkFileNames: '[name]-[hash].mjs',
+        assetFileNames: '[name]-[hash][extname]',
+      },
+    },
+  },
 })
