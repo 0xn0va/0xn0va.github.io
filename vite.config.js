46bed43b30129963fcf59c77c5565729023757c9
BRp99 <124196319+BRp99@users.noreply.github.com>
2025-02-03 13:35:15 +0000

    config.deploy

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio/",
-
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name]-[hash].mjs",
-        chunkFileNames: "[name]-[hash].mjs",
-        assetFileNames: "[name]-[hash][extname]",
-      },
-    },
-  },
 })
