7768031856dd16bdcc68b80e65e365d3bab298d3
BRp99 <124196319+BRp99@users.noreply.github.com>
2025-02-06 18:17:30 +0000

    change base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/BRp99.github.io/",
   build: {
     rollupOptions: {
       output: {
